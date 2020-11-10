6b6ab3af7933b51f5f68c8ee700463a55c190393
MitchWijt <mitchel@wijt.net>
2020-11-10 11:30:02 -0400

    test: add a test case for PUT endpoint student

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ const jsonist = require('jsonist')
 
 const port = (process.env.PORT = process.env.PORT || require('get-port-sync')())
 const endpoint = `http://localhost:${port}`
+const putEndpointFileId = 'rn1abu1'
 
 const server = require('./server')
+const { removeFile } = require('./utils/fileSystem')
 
 tape('should respond dagpauwoog 81', async function (t) {
   const url = `${endpoint}/rn1abu8/info/naw/address`
@@ -24,7 +26,20 @@ tape('should respond not found', async function (t) {
   })
 })
 
+tape('should respond dagpauwoog 90', async function (t) {
+  const url = `${endpoint}/rn1abu1/info/naw`
+  const data = {
+    address: 'dagpauwoog 90'
+  }
+  jsonist.put(url, data, (err, body) => {
+    if (err) t.error(err)
+    t.equal(body.info.naw.address, 'dagpauwoog 90')
+    t.end()
+  })
+})
+
 tape('cleanup', function (t) {
+  removeFile(`data/${putEndpointFileId}.json`)
   server.close()
   t.end()
 })
