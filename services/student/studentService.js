667564bb2dd2220c1dee8b499d0cc6b9940f2f24
MitchWijt <mitchel@wijt.net>
2020-11-10 11:55:49 -0400

    feat: add a DELETE case in service for student endpoint

--- services/student/studentService.js
+++ services/student/studentService.js
@@ -1,5 +1,6 @@
 const { getStudent } = require('./getStudent')
 const { putStudent } = require('./putStudent')
+const { deleteStudent } = require('./deleteStudent')
 
 module.exports = {
   studentService
@@ -9,5 +10,6 @@ async function studentService (httpRequest) {
   switch (httpRequest.method) {
     case 'GET' : return getStudent(httpRequest)
     case 'PUT' : return putStudent(httpRequest)
+    case 'DELETE': return deleteStudent(httpRequest)
   }
 }
