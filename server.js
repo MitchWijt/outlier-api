85accd1e79a816bbbf7d4f4854e2db7ef373768f
MitchWijt <mitchel@wijt.net>
2020-11-10 09:41:59 -0400

    feat: build the architecture for the api

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const api = require('./api')
 const middleware = require('./middleware')
 
 const PORT = process.env.PORT || 1337
@@ -9,8 +8,6 @@ const PORT = process.env.PORT || 1337
 const app = express()
 
 app.use(bodyParser.json())
-
-app.get('/health', api.getHealth)
 
 app.use(middleware.handleError)
 app.use(middleware.notFound)
